aea026a80d516d8e354a47521a63b13326f652ce
i-am-crazy-rabbit <bekki14@bk.ru>
2021-12-04 01:54:27 +0300

    first commit

---
+++ app/src/main/java/crazy/rabbit/practice_first/MainActivity.kt
@@ -0,0 +1,11 @@
+package crazy.rabbit.practice_first
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
